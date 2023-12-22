46971363b9aa876eedf331cf0873378786cdb336
Mizuki-Kawabe <111262336+Mizuki-Kawabe@users.noreply.github.com>
2023-12-22 12:35:40 +1100

    responsive cart

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -31,52 +31,106 @@ const CartClient = () => {
   return (
     <div>
       <Heading title="Shopping Cart" center />
-      <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center mt-8">
-        <div className="col-span-2 justify-start">PRODUCT</div>
-        <div className="justyfy-self-center">PRICE</div>
-        <div className="justyfy-self-center">QUANTITY</div>
-        <div className="justyfy-self-end">TOTAL</div>
-      </div>
-      <div>
-        {cartProducts &&
-          cartProducts.map((item) => {
-            return <ItemContent key={item.id} item={item} />;
-          })}
-      </div>
-      <div className="py-4 flex justify-between gap-4">
-        <div className="w-[90px]">
-          <button
-            className="btn btn-outline text-slate-500"
-            onClick={() => {
-              handleClearCart();
-            }}
-          >
-            ClearCart
-          </button>
+      <div className="md:hidden">
+        <div className="grid grid-cols-3 text-xs gap-4 pb-2 items-center mt-8 border-b-[1.5px] border-slate-200">
+          <div className="justify-start">PRODUCT</div>
+
+          <div className="justify-self-center">QUANTITY</div>
+          <div className="justify-self-end">TOTAL</div>
         </div>
-
-        <div className="text-sm flex flex-col gap-1 items-start">
-          <div className="flex justify-between w-full text-base font-semibold">
-            <span>SubTotal</span>
-            <span>{formatPrice(cartTotalAmount)}</span>
+        <div>
+          {cartProducts &&
+            cartProducts.map((item) => {
+              return <ItemContent key={item.id} item={item} />;
+            })}
+        </div>
+        <div className="py-4">
+          <div className="w-full flex justify-end">
+            <button
+              className="btn btn-outline text-slate-500"
+              onClick={() => {
+                handleClearCart();
+              }}
+            >
+              ClearCart
+            </button>
           </div>
-          <p className="tet-slate-500">
-            Taxes and shipping calculated at checkout
-          </p>
 
-          <button
-            className="btn bg-tan text-white border-none"
-            onClick={() => {}}
-          >
-            Checkout
-          </button>
-          <Link
-            href={"/"}
-            className="text-slate-500 flex items-center gap-1 mt-2"
-          >
-            <MdArrowBack />
-            <span>Go back to home</span>
-          </Link>
+          <div className="text-sm flex flex-col gap-3 items-start mt-10">
+            <div className="flex justify-between w-full text-base font-semibold">
+              <span>SubTotal</span>
+              <span>{formatPrice(cartTotalAmount)}</span>
+            </div>
+            <p className="tet-slate-500">
+              Taxes and shipping calculated at checkout
+            </p>
+            <div className="flex flex-col w-full">
+              <button
+                className="btn bg-tan text-white border-none w-44 m-auto"
+                onClick={() => {}}
+              >
+                Check Out
+              </button>
+              <Link
+                href={"/"}
+                className="text-slate-500 flex items-center gap-1 mt-2"
+              >
+                <MdArrowBack />
+                <span>Go back to home</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* pc ver */}
+      <div className="hidden md:block">
+        <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center mt-8 border-b-[1.5px] border-slate-200">
+          <div className="col-span-2 justify-start">PRODUCT</div>
+          <div className="justify-self-center">PRICE</div>
+          <div className="justify-self-center">QUANTITY</div>
+          <div className="justify-self-end">TOTAL</div>
+        </div>
+        <div>
+          {cartProducts &&
+            cartProducts.map((item) => {
+              return <ItemContent key={item.id} item={item} />;
+            })}
+        </div>
+        <div className="py-4 flex justify-between gap-4">
+          <div>
+            <button
+              className="btn btn-outline text-slate-500"
+              onClick={() => {
+                handleClearCart();
+              }}
+            >
+              Clear Cart
+            </button>
+          </div>
+
+          <div className="text-sm flex flex-col gap-1 items-start">
+            <div className="flex justify-between w-full text-base font-semibold">
+              <span>SubTotal</span>
+              <span>{formatPrice(cartTotalAmount)}</span>
+            </div>
+            <p className="tet-slate-500">
+              Taxes and shipping calculated at checkout
+            </p>
+
+            <button
+              className="btn bg-tan text-white border-none"
+              onClick={() => {}}
+            >
+              Check Out
+            </button>
+            <Link
+              href={"/"}
+              className="text-slate-500 flex items-center gap-1 mt-2"
+            >
+              <MdArrowBack />
+              <span>Go back to home</span>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- app/cart/itemContent.tsx
+++ app/cart/itemContent.tsx
@@ -19,53 +19,108 @@ const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
     handleCartQtyDecrease,
   } = useCart();
   return (
-    <div
-      key={item.id}
-      className="
-grid grid-cols-5 text-xs md:text-sm gap-4 border-t-[1.5px] border-b-[1.5px] border-slate-200 py-4 items-center"
-    >
-      <div className="col-span-2 justify-self-start flex gap-2 md:gap-4">
-        <Link href={`/product/${item.id}`}>
-          <div className="relative w-[70px] aspect-square">
-            <Image
-              src={item.selectedImg.image}
-              alt={item.name}
-              fill
-              className="object-contain"
+    <div>
+      <div className="md:hidden">
+        <div
+          key={item.id}
+          className="
+grid grid-cols-3 text-xs md:text-sm gap-4 border-b-[1.5px] border-slate-200 py-6 items-center"
+        >
+          <div className="justify-self-start grid gap-y-1">
+            <Link href={`/product/${item.id}`}>
+              <div className="relative w-[70px] aspect-square">
+                <Image
+                  src={item.selectedImg.image}
+                  alt={item.name}
+                  fill
+                  className="object-contain"
+                />
+              </div>
+            </Link>
+            <div className="justify-self-center">{formatPrice(item.price)}</div>
+            <div className="flex flex-col justify-between">
+              <div className="w-[70px] mt-1">
+                <button
+                  className="link bg-offWhite text-slate-500"
+                  onClick={() => {
+                    handleRemoveProductFromCart(item);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="justify-self-center">
+            <SetQuantity
+              cartCounter={true}
+              cartProduct={item}
+              handleQtyIncrease={() => {
+                handleCartQtyIncrease(item);
+              }}
+              handleQtyDecrease={() => {
+                handleCartQtyDecrease(item);
+              }}
             />
           </div>
-        </Link>
-        <div className="flex flex-col justify-between">
-          <Link href={`/product/${item.id}`}>{truncateText(item.name)}</Link>
-
-          <div className="w-[70px] mt-1">
-            <button
-              className="link bg-offWhite text-slate-500"
-              onClick={() => {
-                handleRemoveProductFromCart(item);
-              }}
-            >
-              Remove
-            </button>
+          <div className="font-semibold justify-self-end">
+            {formatPrice(item.price * item.quantity)}
           </div>
         </div>
       </div>
+      <div className="hidden md:block">
+        <div
+          key={item.id}
+          className="
+grid grid-cols-5 text-xs md:text-sm gap-4 border-b-[1.5px] border-slate-200 py-4 items-center"
+        >
+          <div className="col-span-2 justify-self-start flex gap-2 md:gap-4">
+            <Link href={`/product/${item.id}`}>
+              <div className="relative w-[70px] aspect-square">
+                <Image
+                  src={item.selectedImg.image}
+                  alt={item.name}
+                  fill
+                  className="object-contain"
+                />
+              </div>
+            </Link>
+            <div className="flex flex-col justify-between">
+              <Link href={`/product/${item.id}`}>
+                {truncateText(item.name)}
+              </Link>
 
-      <div>{formatPrice(item.price)}</div>
-      <div>
-        <SetQuantity
-          cartCounter={true}
-          cartProduct={item}
-          handleQtyIncrease={() => {
-            handleCartQtyIncrease(item);
-          }}
-          handleQtyDecrease={() => {
-            handleCartQtyDecrease(item);
-          }}
-        />
-      </div>
-      <div className="font-semibold">
-        {formatPrice(item.price * item.quantity)}
+              <div className="w-[70px] mt-1">
+                <button
+                  className="link bg-offWhite text-slate-500"
+                  onClick={() => {
+                    handleRemoveProductFromCart(item);
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="justify-self-center">{formatPrice(item.price)}</div>
+          <div className="justify-self-center">
+            <SetQuantity
+              cartCounter={true}
+              cartProduct={item}
+              handleQtyIncrease={() => {
+                handleCartQtyIncrease(item);
+              }}
+              handleQtyDecrease={() => {
+                handleCartQtyDecrease(item);
+              }}
+            />
+          </div>
+          <div className="font-semibold justify-self-end">
+            {formatPrice(item.price * item.quantity)}
+          </div>
+        </div>
       </div>
     </div>
   );
