86b4538c0e843d538a57b4f7f0e853e63e8d36a8
Mizuki-Kawabe <111262336+Mizuki-Kawabe@users.noreply.github.com>
2023-12-23 21:37:52 +1100

    register user function

--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -5,6 +5,11 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useState } from "react";
 import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
+import Router from "next/router";
+import { useRouter } from "next/navigation";
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -20,9 +25,35 @@ const RegisterForm = () => {
     },
   });
 
+  const router = useRouter();
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-    console.log(data);
+
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        toast.success("Account created");
+
+        signIn("credentials", {
+          email: data.email,
+          password: data.password,
+          redirect: false,
+        }).then((callback) => {
+          if (callback?.ok) {
+            router.push("/cart");
+            router.refresh();
+            toast.success("Logged In");
+          }
+          if (callback?.error) {
+            toast.error(callback.error);
+          }
+        });
+      })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
